b817ff9e4c9aa1e8c847c5e6482d84b227c9c695
VilloAn <villo.anna@gmail.com>
2022-05-07 17:51:12 +0200

    Pages

---
+++ Game.WPF/Pages/LevelPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Game.WPF.Pages
+{
+    /// <summary>
+    /// Interaction logic for LevelPage.xaml
+    /// </summary>
+    public partial class LevelPage : Page
+    {
+        public LevelPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Level1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Level2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Level3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Level4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BackToMenu(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.GoBack();
+        }
+    }
+}
---
+++ Game.WPF/Pages/OptionsPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Game.WPF.Pages
+{
+    /// <summary>
+    /// Interaction logic for OptionsPage.xaml
+    /// </summary>
+    public partial class OptionsPage : Page
+    {
+        public OptionsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SkinDark(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SkinWhite(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void volumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+    }
+}
